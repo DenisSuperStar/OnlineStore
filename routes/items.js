a3c1bd6b82bc5567be6b7fbbef97b39d1da93350
Denis <den.shklyarskij@bk.ru>
2021-11-29 03:31:28 +0300

    added new routers

--- routes/items.js
+++ routes/items.js
@@ -5,13 +5,23 @@ const { renderCatalog } = require('../controllers/items/renderItemCatalogControl
 const { renderConfirm } = require('../controllers/items/renderConfirmFormController.js');
 const { processConfirmToUpload } = require('../controllers/items/processConfirmToUploadController.js');
 const { processConfirmToDelete } = require('../controllers/items/processConfirmToDeleteController.js');
+const { renderUpload } = require('../controllers/items/renderUploadFormController.js');
+const { processUpload } = require('../controllers/items/processUploadDataController.js');
+const { renderDelete } = require('../controllers/items/renderDeleteFormController.js');
+const { processDelete } = require('../controllers/items/processDeleteDataController.js');
 
 item.get('/:uId', renderCatalog);
 
 item.get('/upload/confirm', renderConfirm);
 item.post('/upload/confirm', processConfirmToUpload);
 
+item.get('/upload/:uId', renderUpload);
+item.post('/upload/:uId', processUpload);
+
 item.get('/delete/confirm', renderConfirm);
 item.post('/delete/confirm', processConfirmToDelete);
+
+item.get('/delete/:uId/:id', renderDelete);
+item.post('/delete/:uId/:id', processDelete);
 
 module.exports = item;
