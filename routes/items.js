ee80bf6a7e1701302110f447ac56ca593e452526
Denis <den.shklyarskij@bk.ru>
2021-12-01 23:20:14 +0300

    rename routes application

--- routes/items.js
+++ routes/items.js
@@ -3,23 +3,23 @@ const item = Router();
 
 const { renderCatalog } = require('../controllers/items/renderItemCatalogController.js');
 const { renderConfirm } = require('../controllers/items/renderConfirmFormController.js');
-const { processConfirmToUpload } = require('../controllers/items/processConfirmToUploadController.js');
+const { processConfirmToScope } = require('../controllers/items/processConfirmToUploadController.js');
 const { processConfirmToDelete } = require('../controllers/items/processConfirmToDeleteController.js');
-const { renderUpload } = require('../controllers/items/renderUploadFormController.js');
-const { processUpload } = require('../controllers/items/processUploadDataController.js');
+const { renderScope } = require('../controllers/items/renderUploadFormController.js');
+const { processScope } = require('../controllers/items/processUploadDataController.js');
 const { renderDelete } = require('../controllers/items/renderDeleteFormController.js');
 const { processDelete } = require('../controllers/items/processDeleteDataController.js');
 const { renderAttach } = require('../controllers/items/renderAttachFormController.js');
 
 item.get('/:uId', renderCatalog);
 
-item.get('/upload/confirm', renderConfirm);
-item.post('/upload/confirm', processConfirmToUpload);
+item.get('/scope/confirm', renderConfirm);
+item.post('/scope/confirm', processConfirmToScope);
 
-item.get('/upload/:uId/', renderUpload);
-item.post('/upload/:uId/', processUpload);
+item.get('/scope/:uId/', renderScope);
+item.post('/scope/:uId/', processScope);
 
-item.get('/upload/:id', renderAttach);
+item.get('/scope/:id', renderAttach);
 
 item.get('/delete/confirm', renderConfirm);
 item.post('/delete/confirm', processConfirmToDelete);
