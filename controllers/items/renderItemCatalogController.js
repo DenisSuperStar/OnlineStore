db9d7eb0bbfb0c25d9555e23be65d16aaac3f530
Denis <den.shklyarskij@bk.ru>
2021-12-03 13:20:38 +0300

    searched items and display their

--- controllers/items/renderItemCatalogController.js
+++ controllers/items/renderItemCatalogController.js
@@ -1,5 +1,18 @@
+const path = require('path');
+const itemFilePath = path.join(__dirname, '../../service/items.json');
+const { readFileToPromise } = require('../../config/toPromise');
+const { storeFill } = require('../../config/fillingStore');
+
 module.exports.renderCatalog = (req, res) => {
-  res.render("home", {
-    title: "Каталог товаров.",
-  });
+  readFileToPromise(itemFilePath)
+    .then(fileToItems => {
+      const items = JSON.parse(fileToItems);
+      const isItemSome = items.some(storeFill);
+
+      res.render('home', {
+        title: 'Каталог товаров',
+        items,
+        isItemSome 
+      });
+    });
 };
