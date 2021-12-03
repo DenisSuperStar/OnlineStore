8f5465c5da1f42c8315a6a9f6e24640268fde8fd
Denis <den.shklyarskij@bk.ru>
2021-12-03 15:29:50 +0300

    found data about items and show it

--- controllers/items/renderUploadFormController.js
+++ controllers/items/renderUploadFormController.js
@@ -1,5 +1,18 @@
+const path = require('path');
+const itemFilePath = path.join(__dirname, '../../service/items.json');
+const { readFileToPromise } = require('../../config/toPromise');
+const { storeFill } = require('../../config/fillingStore');
+
 module.exports.renderScope = (req, res) => {
-  res.render("scope", {
-    title: "Управление товарами.",
+  readFileToPromise(itemFilePath)
+  .then(dataAllItems => {
+    const items = JSON.parse(dataAllItems);
+    const isItemSome = items.some(storeFill);
+
+    res.render('scope', {
+      title: 'Управление товарами.',
+      items,
+      isItemSome
+    });
   });
 };
