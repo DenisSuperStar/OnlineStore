fef8e06f15f5bbd1fda18c8958ba2f15f0ce8a56
Denis <den.shklyarskij@bk.ru>
2021-12-05 02:07:26 +0300

    realized attach file to the item

--- controllers/app/processAttachFile.js
+++ controllers/app/processAttachFile.js
@@ -1,22 +1,26 @@
-const path = require("path");
-const { readFileToPromise } = require("../../config/toPromise");
-const { resizeImage } = require("../../config/resizeImageFile");
+const path = require('path');
+const fs = require('fs');
+const { readFileToPromise } = require('../../config/toPromise');
 const itemFilePath = path.join(__dirname, "../../service/items.json");
 
 module.exports.processAttach = (req, res) => {
   const { file, body } = req;
+  console.log(file);
+  console.log(body);
+  const { originalname } = file;
   const { itemId } = JSON.parse(JSON.stringify(body));
+  console.log(itemId);
 
-  if (!file) return res.redirect(`/upload/${itemId}`);
+  readFileToPromise(itemFilePath)
+    .then(fileToItems => {
+      const items = JSON.parse(fileToItems);
+      const item = items.find(item => item._id == itemId);
+      
+      item.imgPath = `../../${originalname}`;
 
-  readFileToPromise(itemFilePath).then((res) => {
-    const items = JSON.parse(res);
-    const item = items.find((item) => item._id == itemId);
-    const { originalname } = file;
-    const attachFilePath = `./${attachFileFolder}/${originalname}`;
+      const convertItemsToFile = JSON.stringify(items, null, 4);
+      fs.writeFileSync(itemFilePath, convertItemsToFile);
 
-    resizeImage(attachFilePath);
-
-    item.attach = path.join(__dirname, attachFilePath);
-  });
-};
+      res.redirect('/');
+    });
+}
