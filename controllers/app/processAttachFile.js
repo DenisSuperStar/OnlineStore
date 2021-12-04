86fd294a822e9ad972cc7fdf9f10a0551870e6e9
Denis <den.shklyarskij@bk.ru>
2021-12-05 02:19:40 +0300

    view prev commit

--- controllers/app/processAttachFile.js
+++ controllers/app/processAttachFile.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const fs = require('fs');
-const { readFileToPromise } = require('../../config/toPromise');
+const path = require("path");
+const fs = require("fs");
+const { readFileToPromise } = require("../../config/toPromise");
 const itemFilePath = path.join(__dirname, "../../service/items.json");
 
 module.exports.processAttach = (req, res) => {
@@ -11,16 +11,15 @@ module.exports.processAttach = (req, res) => {
   const { itemId } = JSON.parse(JSON.stringify(body));
   console.log(itemId);
 
-  readFileToPromise(itemFilePath)
-    .then(fileToItems => {
-      const items = JSON.parse(fileToItems);
-      const item = items.find(item => item._id == itemId);
-      
-      item.imgPath = `../../${originalname}`;
+  readFileToPromise(itemFilePath).then((fileToItems) => {
+    const items = JSON.parse(fileToItems);
+    const item = items.find((item) => item._id == itemId);
 
-      const convertItemsToFile = JSON.stringify(items, null, 4);
-      fs.writeFileSync(itemFilePath, convertItemsToFile);
+    item.imgPath = `../../${originalname}`;
 
-      res.redirect('/');
-    });
-}
+    const convertItemsToFile = JSON.stringify(items, null, 4);
+    fs.writeFileSync(itemFilePath, convertItemsToFile);
+
+    res.redirect("/");
+  });
+};
