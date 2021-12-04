95c591f13f32ff166be45dea2ff0bbb20cd37999
Denis <den.shklyarskij@bk.ru>
2021-12-05 02:09:26 +0300

    added functions of multer settings

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const multer = require("multer");
 const user = require("./routes/users");
 const item = require("./routes/items");
 
-const attachFileFolder = "uploads";
-const upload = multer({ dest: `${attachFileFolder}/` });
-
 const { start } = require("./config/startServer");
 const { PORT } = require("./config/appConfig");
 
@@ -17,13 +14,29 @@ const { processAttach } = require("./controllers/app/processAttachFile");
 
 const app = express();
 
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const fileSearchFolder = "uploads";
+
+    cb(null, fileSearchFolder);
+  },
+  filename: (req, file, cb) => {
+    const { originalname } = file;
+
+    cb(null, originalname);
+  },
+});
+
+const multerConfig = multer({ storage: storageConfig }).single("attachFile");
+
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(upload.single("attachFile"));
+app.use(multerConfig);
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/uploads'));
 
 app.use("/user", user);
 app.use("/item", item);
