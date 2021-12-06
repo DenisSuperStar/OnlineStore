9529671ac1f77faff7bfe6e7686e5132f60c8b52
Denis <den.shklyarskij@bk.ru>
2021-12-06 16:08:13 +0300

    created new controller of the view with validation and confirmation of user data

--- routes/items.js
+++ routes/items.js
@@ -9,40 +9,44 @@ const {
 } = require("../controllers/items/renderConfirmFormController.js");
 const {
   processConfirmToScope,
-} = require("../controllers/items/processConfirmToUploadController.js");
+} = require("../controllers/items/processConfirmToScopeController.js");
 const {
   processConfirmToDelete,
 } = require("../controllers/items/processConfirmToDeleteController.js");
 const {
   renderScope,
-} = require("../controllers/items/renderUploadFormController.js");
+} = require("../controllers/items/renderScopeFormController.js");
 const {
   processScope,
-} = require("../controllers/items/processUploadDataController.js");
+} = require("../controllers/items/processScopeDataController.js");
 const {
   renderDelete,
 } = require("../controllers/items/renderDeleteFormController.js");
 const {
-  processDelete,
-} = require("../controllers/items/processDeleteDataController.js");
+  processDeleteToConfirm,
+} = require("../controllers/items/processDeleteToConfirmController");
 const {
   renderAttach,
 } = require("../controllers/items/renderAttachFormController.js");
+
+const {
+  renderValidate,
+} = require("../controllers/items/renderValidateController");
 
 item.get("/:uId", renderCatalog);
 
 item.get("/scope/confirm", renderConfirm);
 item.post("/scope/confirm", processConfirmToScope);
 
-item.get("/scope/:uId/", renderScope);
-item.post("/scope/:uId/", processScope);
+item.get("/scope/:uId", renderScope);
+item.post("/scope/:uId", processScope);
 
 item.get("/upload/:uId/:id", renderAttach);
 
-item.get("/delete/confirm", renderConfirm);
-item.post("/delete/confirm", processConfirmToDelete);
+item.get("/delete/:id", renderDelete);
+item.post("/delete/:id", processDeleteToConfirm);
 
-item.get("/delete/:uId/:id", renderDelete);
-item.post("/delete/:uId/:id", processDelete);
+item.get("/delete/confirm/:id", renderValidate);
+item.post("/delete/confirm", processConfirmToDelete);
 
 module.exports = item;
