58953e8180a36a5eff84e0635c5af9798b0722d5
Denis <den.shklyarskij@bk.ru>
2021-12-06 14:50:00 +0300

    reformatted attach file code

--- controllers/app/processAttachFile.js
+++ controllers/app/processAttachFile.js
@@ -1,15 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 const { readFileToPromise } = require("../../config/toPromise");
+
 const itemFilePath = path.join(__dirname, "../../service/items.json");
 
 module.exports.processAttach = (req, res) => {
   const { file, body } = req;
-  console.log(file);
-  console.log(body);
   const { originalname } = file;
   const { itemId } = JSON.parse(JSON.stringify(body));
-  console.log(itemId);
 
   readFileToPromise(itemFilePath).then((fileToItems) => {
     const items = JSON.parse(fileToItems);
