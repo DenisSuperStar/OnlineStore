f35abf8ae0788da15597027b1edce02c6f931f1b
Denis <den.shklyarskij@bk.ru>
2021-12-12 14:41:23 +0300

    rename function in this file

--- functions/maySaveItem.js
+++ functions/maySaveItem.js
@@ -1,4 +1,4 @@
-export const isSaveItem = (body) => {
+export const readySaveItem = (body) => {
   const { itemName, size, stuff, price } = JSON.parse(JSON.stringify(body));
 
   const isSaveItem = itemName && size && stuff && price ? true : false;
