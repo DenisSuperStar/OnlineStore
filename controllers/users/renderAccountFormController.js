c57fdbb7b7d07630898c0fca548201c88798e1fd
Denis <den.shklyarskij@bk.ru>
2021-12-17 00:37:44 +0300

    removed calculation of public ip

--- controllers/users/renderAccountFormController.js
+++ controllers/users/renderAccountFormController.js
@@ -1,11 +1,3 @@
-import { v4 } from "public-ip";
-
-// использовать эту функцию для того, чтобы скрыть страницу удаление товара
-(async () => {
-	let myIp = await v4();
-  console.log(myIp);
-})();
-
 export const renderAccount = (req, res) => {
   res.render("account", {
     title: "Быстрая регистрация.",
