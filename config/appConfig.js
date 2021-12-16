60240b084765f450dcb16b2d45f66e3f095da2f8
Denis <den.shklyarskij@bk.ru>
2021-12-17 00:29:57 +0300

    added public ip application settings

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,5 +1,7 @@
 const settings = {
-    PORT: 3000
+    PORT: process.env.APP_PORT || 8000,
+    AdminIp: process.env.ADMIN_IP,
+    CustomerIp: process.env.CUSTOMER_IP
 }
 
 export default settings;
