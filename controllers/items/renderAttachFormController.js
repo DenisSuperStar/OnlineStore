45bcbe70100f487b29968c2c310199ddca0fa3d1
Denis <den.shklyarskij@bk.ru>
2021-12-21 16:57:50 +0300

    added validate with pubicIp and remove user id

--- controllers/items/renderAttachFormController.js
+++ controllers/items/renderAttachFormController.js
@@ -4,18 +4,28 @@ import { ReasonPhrases } from "http-status-codes";
 
 import { readFileToPromise } from "../../functions/toPromise";
 import { findItem } from "../../functions/findItemById";
+import { getPublicIp } from "../../functions/getPublicIp";
+import { getParsedEnv } from "../../config/envConfig";
 
 const __dirname = path.resolve();
 const itemFilePath = path.join(__dirname, "/service/items.json");
+const { ADMIN_IP, CUSTOMER_IP } = getParsedEnv();
 
-export const renderAttach = (req, res) => {
+export const renderAttach = async (req, res) => {
   const { id } = req.params;
   const isId = validate(id);
 
   if (isId) {
-    readFileToPromise(itemFilePath).then((itemData) => {
-      findItem(res, id, itemData, "attach", "Прикрепить изображение.");
-    });
+
+    const publicIp = await getPublicIp();
+
+    if ((publicIp == ADMIN_IP) || (publicIp == CUSTOMER_IP)) {
+      readFileToPromise(itemFilePath).then((itemData) => {
+        findItem(res, id, itemData, "attach", "Прикрепить изображение.");
+      });
+    } else {
+      res.send(ReasonPhrases.FORBIDDEN);
+    }
   } else {
     res.send(ReasonPhrases.NOT_FOUND);
   }
