7a76f1cea2b02944db2bc8d77b6b935041c26199
Denis <den.shklyarskij@bk.ru>
2021-12-25 21:00:56 +0300

    converted css styles to css modules

---
+++ public/extended/searchItem.js
@@ -0,0 +1,30 @@
+const search = document.getElementById("search");
+const cards = document.querySelectorAll("#card");
+
+search.addEventListener("input", function () {
+  const query = this.value.toLowerCase();
+
+  cards.forEach((card) => {
+    let price = card.querySelector("#itemPrice");
+    let name = card.querySelector("#itemName");
+
+    if (
+      !(
+        price.innerHTML.toLowerCase().indexOf(query) > -1 ||
+        name.innerHTML.toLowerCase().indexOf(query) > -1
+      )
+    ) {
+      card.parentElement.classList.add("is-hidden");
+    } else {
+    }
+  });
+});
+
+search.addEventListener("input", function () {
+  const query = this.value;
+  if (query == "") {
+    cards.forEach((card) => {
+      card.parentElement.classList.remove("is-hidden");
+    });
+  }
+});
---
+++ public/fieldSearch/fieldSearch.js
@@ -0,0 +1,25 @@
+import { styleModule } from "https://unpkg.com/style-module?module";
+
+const searchFieldStyles = styleModule({
+  search: {
+    width: "50%",
+    height: "56px",
+    borderRadius: "25px",
+    background: 'linear-gradient(85deg, #0d6efd, #dc3545)',
+    opacity: 0.8,
+    border: "none",
+    color: "#fff",
+    cursor: "pointer",
+    transition: "background ease-in-out .5s",
+  },
+});
+
+const fieldSearch = document.getElementById("search");
+
+fieldSearchStylish(searchFieldStyles, fieldSearch);
+
+function fieldSearchStylish(searchStyles, field) {
+  const { search } = searchStyles;
+
+  field.classList.add(search);
+}
