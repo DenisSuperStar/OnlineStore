e2fa76a7052410401bcfaaa01e796c010282815c
Denis <den.shklyarskij@bk.ru>
2021-12-30 14:38:08 +0300

    renaming function of some routes

--- routes/items.js
+++ routes/items.js
@@ -1,26 +1,25 @@
 import { Router } from "express";
 
-import { processConfirmToDelete } from "../controllers/items/processConfirmToDeleteController";
-import { processDeleteToConfirm } from "../controllers/items/processDeleteToConfirmController";
+import { renderCatalog } from "../controllers/items/renderItemCatalogController";
+import { renderScope } from "../controllers/items/renderScopeFormController";
 import { processScope } from "../controllers/items/processScopeDataController";
 import { renderAttach } from "../controllers/items/renderAttachFormController";
 import { renderDelete } from "../controllers/items/renderDeleteFormController";
-import { renderCatalog } from "../controllers/items/renderItemCatalogController";
-import { renderScope } from "../controllers/items/renderScopeFormController";
 import { renderValidate } from "../controllers/items/renderValidateController";
+import { confirmPersonToDelete } from "../controllers/items/processConfirmToDeleteController";
 
 const item = Router();
 
 item.get("/public/:uId", renderCatalog);
 
-item.get('/access/private', renderScope);
-item.post('/access/private', processScope);
+item.get("/access/private", renderScope);
+item.post("/access/private", processScope);
 
 item.get("/upload/:id", renderAttach);
 
 item.get("/delete/:id", renderDelete);
 
 item.get("/delete/confirm/:id", renderValidate);
-item.post("/delete/confirm/:id", processConfirmToDelete);
+item.post("/delete/confirm/:id", confirmPersonToDelete);
 
 export default item;
