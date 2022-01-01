8289c608fcd3d94e519f6dc4f24ccb10713dc035
Denis <den.shklyarskij@bk.ru>
2022-01-01 11:09:24 +0300

    modified saveUserToLocalStorage function

--- functions/saveUserToLocalStorage.js
+++ functions/saveUserToLocalStorage.js
@@ -4,7 +4,6 @@ const localStorage = new LocalStorage("./scratch");
 export const saveUserToLocalStorage = (currentUser) => {
   const { _id } = currentUser;
   const currentUserItem = JSON.stringify(currentUser);
-  localStorage.setItem(_id, currentUserItem);
 
-  return _id;
+  localStorage.setItem(_id, currentUserItem);
 };
