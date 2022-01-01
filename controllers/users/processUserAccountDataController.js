d75a3dc961531b9a94a12de86583d55c37926cf4
Denis <den.shklyarskij@bk.ru>
2022-01-01 09:27:16 +0300

    imported saveUserToLocalStorage

--- controllers/users/processUserAccountDataController.js
+++ controllers/users/processUserAccountDataController.js
@@ -1,36 +1,31 @@
-import path from "path";
-
-import { readFileToPromise } from "../../functions/toPromise";
 import { checkAccount } from "../../functions/checkUserAccount";
 import { createUser } from "../../functions/createNewUser";
-import { saveUser } from "../../functions/saveUserToDb";
-
-const __dirname = path.resolve();
-const userFilePath = path.join(__dirname, "/service/users.json");
+import { saveUserToLocalStorage } from "../../functions/saveUserToLocalStorage";
+import { LocalStorage } from "node-localstorage";
+const localStorage = new LocalStorage("./scratch");
 
 export const processAccount = async (req, res) => {
   const { body } = req;
 
   if (checkAccount(body)) {
-    readFileToPromise(userFilePath).then((fileToUsers) => {
-      const users = JSON.parse(fileToUsers);
-      const currentUser = users.find(
-        (user) =>
-          user.firstName == body.firstName && user.lastName == body.lastName
-      );
+    for (let i = 0; i < localStorage.length; i++) {
+      let userKey = localStorage.key(i);
+      let user = JSON.parse(localStorage.getItem(userKey));
+      var isUserSearched =
+        user.firstName == body.firstName && user.lastName == body.lastName;
 
-      if (!currentUser) {
-        let currentUser = new Object();
+      if (isUserSearched) break;
+    }
 
-        currentUser = createUser(body, currentUser);
-        saveUser(currentUser, users, userFilePath);
-        let id = saveUserToSessionStorage(currentUser);
+    if (!isUserSearched) {
+      let createdUser = new Object();
+      let currentUser = await createUser(body, createdUser);
+      let id = await saveUserToLocalStorage(currentUser);
 
-        res.redirect(`/item/public/${id}`);
-      } else {
-        res.redirect("/user/auth");
-      }
-    });
+      res.redirect(`/item/public/${id}`);
+    } else {
+      res.redirect("/user/auth");
+    }
   } else {
     res.redirect("/user/account");
   }
