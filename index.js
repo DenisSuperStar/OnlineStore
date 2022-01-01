d45d91f73e05e35d5b68cec3f0def35f99ef5763
Denis <den.shklyarskij@bk.ru>
2022-01-01 11:13:31 +0300

    added cookie parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import multer from "multer";
 import { ReasonPhrases } from "http-status-codes";
+import cookieParser from "cookie-parser";
 import { getParsedEnv } from "./config/envConfig";
 
 import user from "./routes/users";
@@ -26,6 +27,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(multerConfig);
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static(path.join(__dirname, "/uploads")));
 app.use(express.static(path.join(__dirname, "/resize")));
