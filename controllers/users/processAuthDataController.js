625a13fdd527eea69de011e43add427f9b41d59c
Denis <den.shklyarskij@bk.ru>
2022-01-01 17:10:37 +0300

    updated, remove set header, simple res.cokie, working

--- controllers/users/processAuthDataController.js
+++ controllers/users/processAuthDataController.js
@@ -10,11 +10,7 @@ export const processAuth = (req, res) => {
     const { _id } = searchedUserAutorize;
 
     if (autorizeUserForId(_id)) {
-      res.setHeader("Set-cookie", res.cookie("_id", _id), {
-        maxAge: 3600 * 24,
-        httpOnly: true,
-        signed: true,
-      });
+      res.cookie("_id", _id);
       res.redirect("/item/public");
     } else {
       res.redirect("/user/account");
